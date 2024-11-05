c48a255373b98fa65be25ff2bcc07a357e920b8b
IriemDlzGlnd <yenduuca2005@gmail.com>
2024-11-05 13:02:41 +0000

    Add files

---
+++ CoroutinesZeroToHero/app/src/main/java/org/iesharia/coroutineszerotohero/RetrofitHelper.kt.kt
@@ -0,0 +1,20 @@
+
+package org.iesharia.coroutineszerotohero
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+object `RetrofitHelper.kt` {
+    private const val URL = "https://superheroapi.com"
+
+    private val retrofit =
+        Retrofit
+            .Builder()
+            .baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+
+    fun getInstance():ApiServices{
+        return retrofit.create(ApiServices::class.java)
+    }
+}
